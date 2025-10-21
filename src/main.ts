dc60840687f175300ced326210716a3d07fc7169
Javier <it@navadriving.com>
2025-10-21 10:51:00 -0600

    se regresa configuración original al main

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Permitir todos los orígenes (CORS abierto)
-  app.enableCors({
-    origin: '*', // o (origin: true para reflejar el origen y permitir credenciales)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: '*',
-    exposedHeaders: 'Content-Disposition',
-  });
+  // Habilita CORS SOLO fuera de producción
+  if (process.env.NODE_ENV !== 'production') {
+    app.enableCors({
+      origin: [
+        'http://localhost:8080',
+        'http://127.0.0.1:8080',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      credentials: true,
+      exposedHeaders: 'Content-Disposition', // útil si devuelves descargas
+      maxAge: 86400,
+    });
+  }
 
   await app.listen(process.env.PORT || 5000);
 }
